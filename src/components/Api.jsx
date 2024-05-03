7e7d10c6540b7f3605669f7733d54b8a3ddea166
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-05-03 14:44:36 +0200

    030520243

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -2,56 +2,61 @@ import axios from 'axios';
 
 const APIKEY = 'e41d1e074713eb19259eb796492b59d1';
 
-const Api = () => {
-  // повернення популярних фільмів
-  const fetchTrendingMovies = async () => {
-    const target = `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIKEY}`;
-    const response = await axios.get(target);
-    // цей вираз повертає частину даних, а саме результати пошуку.
-    // корисний, коли я шукаю фільм за запитом
-    // в даному випадку цей вираз повертає популярні фільми
-    return response.data.results;
-  };
+// повернення популярних фільмів
+export const fetchTrendingMovies = async () => {
+  const target = `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIKEY}`;
+  const response = await axios.get(target);
+  // цей вираз повертає частину даних, а саме результати пошуку.
+  // корисний, коли я шукаю фільм за запитом
+  // в даному випадку цей вираз повертає популярні фільми
+  return response.data.results;
+};
 
-  // повернення фільмів за пошуком
-  const fetchSearchQuery = async query => {
-    const target = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}&include_adult=false&language=en-US&page=1`;
-    const response = await axios.get(target);
-    // цей вираз повертає частину даних, а саме результати пошуку.
-    // корисний, коли я шукаю фільм за запитом
-    return response.data.results;
-  };
+// повернення фільмів за пошуком
+export const fetchSearchQuery = async query => {
+  const target = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}&include_adult=false&language=en-US&page=1`;
+  const response = await axios.get(target);
+  // цей вираз повертає частину даних, а саме результати пошуку.
+  // корисний, коли я шукаю фільм за запитом
+  return response.data.results;
+};
 
-  //   повернення деталей про вибраний фільм
-  const fetchDetailsMovie = async id => {
-    const target = `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}&language=en-US`;
-    const response = await axios.get(target);
-    // вираз повертає ВСІ дані, які я отримала з API.
-    // корисно, коли треба отримати загальне інфо про фільм
-    // (заголовок, опис, рік випуску, рейтинг ітд)
-    return response.data;
-  };
+//   повернення деталей про вибраний фільм
+export const fetchDetailsMovie = async id => {
+  const target = `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}&language=en-US`;
+  const response = await axios.get(target);
+  // вираз повертає ВСІ дані, які я отримала з API.
+  // корисно, коли треба отримати загальне інфо про фільм
+  // (заголовок, опис, рік випуску, рейтинг ітд)
+  return response.data;
+};
 
-  // повернення груп акторів, які грали у вібраному фільмі
-  const fetchCastMovie = async id => {
-    const target = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${APIKEY}&language=en-US`;
+// повернення груп акторів, які грали у вібраному фільмі
+export const fetchCastMovie = async id => {
+  const target = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${APIKEY}&language=en-US`;
 
-    const response = await axios.get(target);
-    // цей вираз повертає ЛИШЕ частину даних,
-    // в даному випадку тільки групу акторів, які грали у
-    // вибраному фільмі
-    return response.data.cast;
-  };
+  const response = await axios.get(target);
+  // цей вираз повертає ЛИШЕ частину даних,
+  // в даному випадку тільки групу акторів, які грали у
+  // вибраному фільмі
+  return response.data.cast;
+};
 
-  //   повернення рецензії вибраного фільму
-  const fetchReviewsMovie = async review_id => {
-    const target = `https://api.themoviedb.org/3/review/${review_id}?api_key=${APIKEY}`;
-    const response = await axios.get(target);
-    // вираз повертає ВСІ дані, які я отримала з API.
-    // корисно, коли треба отримати загальне інфо про фільм
-    // (заголовок, опис, рік випуску, рейтинг ітд)
-    return response.data;
-  };
+//   повернення рецензії вибраного фільму
+export const fetchReviewsMovie = async review_id => {
+  const target = `https://api.themoviedb.org/3/review/${review_id}?api_key=${APIKEY}`;
+  const response = await axios.get(target);
+  // вираз повертає ВСІ дані, які я отримала з API.
+  // корисно, коли треба отримати загальне інфо про фільм
+  // (заголовок, опис, рік випуску, рейтинг ітд)
+  return response.data;
+};
+const Api = {
+  fetchTrendingMovies,
+  fetchSearchQuery,
+  fetchDetailsMovie,
+  fetchCastMovie,
+  fetchReviewsMovie,
 };
 
 export default Api;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,36 @@
-const Home = () => {};
+import { fetchTrendingMovies } from 'components/Api';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+const Home = () => {
+  const [trending, setTrending] = useState([]);
+  const seeTrending = async () => {
+    try {
+      const fetchedMovies = await fetchTrendingMovies();
+      setTrending(fetchedMovies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    seeTrending();
+  }, []);
+  return (
+    <>
+      <h1>Trending today</h1>
+      <ul>
+        {trending.map(movie => {
+          return (
+            <li key={movie.id}>
+              <Link to={`movies/${movie.id}`} state={{ from: '/' }}>
+                {movie.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default Home;
