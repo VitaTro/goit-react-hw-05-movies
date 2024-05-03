e244309f082d4ac58c8c498738b90a0cec7e9666
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-05-03 09:46:09 +0200

    03052024

---
+++ src/components/Api.jsx
@@ -0,0 +1,5 @@
+const APIKEY = 'e41d1e074713eb19259eb796492b59d1';
+
+const Api = () => {};
+
+export default Api;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import { Route, Routes } from 'react-router-dom';
+import Cast from './Cast/Cast';
+import Home from './Home/Home';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Movies from './Movies/Movies';
+import Reviews from './Reviews/Reviews';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="//movies/:movieId" element={<MovieDetails />} />
+        <Route path="//movies/:movieId/cast" element={<Cast />} />
+        <Route path="//movies/:movieId/reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 };
+export default App;
