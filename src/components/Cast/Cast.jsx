972e0d417e66ba092a60b5dae55f83d4b94bb19f
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-05-05 13:41:38 +0200

    050520242

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -3,34 +3,42 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Cast = () => {
-  const [cast, setCast] = useState();
+  const [cast, setCast] = useState([]);
+
   const showFetchedCastMovies = async id => {
-    const fetchedCastMovie = await fetchCastMovie(id);
-    setCast(fetchedCastMovie);
+    try {
+      const fetchedCastMovie = await fetchCastMovie(id);
+      setCast(fetchedCastMovie);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   const { movieId } = useParams();
+
   useEffect(() => {
     showFetchedCastMovies(movieId);
-  }, []);
-  if (cast) {
+  }, [movieId]);
+
+  if (cast && cast.length > 0) {
     return (
       <div>
         <ul>
-          {cast.map(e => {
-            return (
-              <li key={e.id}>
-                <img src={e.profile_path} alt={e.name} />
-                <div>
-                  <h3>Additional information</h3>
-                  <p>Name:{e.name}</p>
-                  <p>Character:{e.character}</p>
-                </div>
-              </li>
-            );
-          })}
+          {cast.map(e => (
+            <li key={e.id}>
+              <img src={e.profile_path} alt={e.name} />
+              <div>
+                <h3>Additional information</h3>
+                <p>Name: {e.name}</p>
+                <p>Character: {e.character}</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     );
+  } else {
+    return <div>Loading...</div>;
   }
 };
 
